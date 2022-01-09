e4607c9c9c4f4184a2fdb5ba73a05f9b9e7437b6
MarvinOostland <74358348+MarvinOostland@users.noreply.github.com>
2022-01-09 15:18:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ class Settings(object):
 
 class Background(object):
     def __init__(self, filename):
-        self.image = pygame.image.load(os.path.join(
-            Settings.path_image, filename))
+        self.image = pygame.image.load(os.path.join(Settings.path_image, filename))
         self.image = pygame.transform.scale(
-            self.image, (Settings.window_width, Settings.window_height))
+            self.image, (Settings.window_width, Settings.window_height)
+        )
         self.rect = self.image.get_rect()
 
     def draw(self, screen):
@@ -35,18 +35,24 @@ class Bubble(pygame.sprite.Sprite):
     def __init__(self, filename, g):
         super().__init__()
         self.r = 10
-        self.image_orig = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
+        self.image_orig = pygame.image.load(
+            os.path.join(Settings.path_image, filename)
+        ).convert_alpha()
         self.image = pygame.transform.scale(self.image_orig, (self.r * 2, self.r * 2))
         self.rect = self.image.get_rect()  # Hitbox
         self.find_position(g)
         self.t = 0
         self.spawnrate = 0
-        self.scale = {'width': self.rect.width, 'height': self.rect.height}
+        self.scale = {"width": self.rect.width, "height": self.rect.height}
         self.scale_speed = random.randint(1, 4)
 
     def find_position(self, g):
-        x1 = random.randint(self.r + Settings.border, Settings.window_width - self.r - Settings.border)
-        y1 = random.randint(self.r + Settings.border, Settings.window_height - self.r - Settings.border)
+        x1 = random.randint(
+            self.r + Settings.border, Settings.window_width - self.r - Settings.border
+        )
+        y1 = random.randint(
+            self.r + Settings.border, Settings.window_height - self.r - Settings.border
+        )
         r1 = self.r
         if len(g.bubbles) <= 0:
             self.rect.center = (x1, y1)
@@ -62,19 +68,26 @@ class Bubble(pygame.sprite.Sprite):
         self.find_position(g)
 
     def update(self):
-        self.scale['width'] += self.scale_speed
-        self.scale['height'] += self.scale_speed
+        self.scale["width"] += self.scale_speed
+        self.scale["height"] += self.scale_speed
         c = self.rect.center
-        self.image = pygame.transform.scale(self.image_orig, (self.scale['width'], self.scale['height']))
+        self.image = pygame.transform.scale(
+            self.image_orig, (self.scale["width"], self.scale["height"])
+        )
         self.rect = self.image.get_rect()
         self.rect.center = c
-        collision_bubbles = pygame.sprite.spritecollide(self, game.bubbles, False, pygame.sprite.collide_mask)
+        collision_bubbles = pygame.sprite.spritecollide(
+            self, game.bubbles, False, pygame.sprite.collide_mask
+        )
         if len(collision_bubbles) > 0:
             for bubble in collision_bubbles:
                 if bubble != self:
-                    collision_sound = pygame.mixer.Sound(os.path.join(Settings.path_sound, "Collision.mp3"))
+                    collision_sound = pygame.mixer.Sound(
+                        os.path.join(Settings.path_sound, "Collision.mp3")
+                    )
                     pygame.mixer.Sound.play(collision_sound)
                     game.gameover = True
+
 
 class Mouse(pygame.sprite.Sprite):
     def __init__(self, filename="Pinnadel2.png"):
@@ -85,7 +98,9 @@ class Mouse(pygame.sprite.Sprite):
         screen.blit(self.image, pygame.mouse.get_pos())
 
     def change_image(self, filename):
-        self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
+        self.image = pygame.image.load(
+            os.path.join(Settings.path_image, filename)
+        ).convert_alpha()
         self.image = pygame.transform.scale(self.image, (30, 35))
         self.rect = self.image.get_rect()
 
@@ -93,7 +108,9 @@ class Mouse(pygame.sprite.Sprite):
 class Game(object):
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((Settings.window_width, Settings.window_height))
+        self.screen = pygame.display.set_mode(
+            (Settings.window_width, Settings.window_height)
+        )
         self.clock = pygame.time.Clock()
         self.running = True
         self.background = Background("underwater.jpg")
@@ -127,29 +144,59 @@ class Game(object):
         screenfill.set_alpha(180)
         self.screen.blit(screenfill, (0, 0))
         pause = self.font_normalsize.render("Paused", False, (255, 255, 255))
-        score = self.font_normalsize.render(f"Highscore:{self.get_highscore()}", False, (255, 255, 255))
-        self.screen.blit(pause, (
-        Settings.window_width // 2 - pause.get_width() // 2, Settings.window_height // 2 - pause.get_height() // 2))
-        self.screen.blit(score, (
-        Settings.window_width // 2 - score.get_width() // 2, Settings.window_height // 2.1 - score.get_height() // 2.1))
+        score = self.font_normalsize.render(
+            f"Highscore:{self.get_highscore()}", False, (255, 255, 255)
+        )
+        self.screen.blit(
+            pause,
+            (
+                Settings.window_width // 2 - pause.get_width() // 2,
+                Settings.window_height // 2 - pause.get_height() // 2,
+            ),
+        )
+        self.screen.blit(
+            score,
+            (
+                Settings.window_width // 2 - score.get_width() // 2,
+                Settings.window_height // 2.1 - score.get_height() // 2.1,
+            ),
+        )
 
     def gameover_check(self):
-        gameover_screenfill = pygame.Surface((Settings.window_width, Settings.window_height))
-        gameover_screenfill.fill((170,170,170))
+        gameover_screenfill = pygame.Surface(
+            (Settings.window_width, Settings.window_height)
+        )
+        gameover_screenfill.fill((170, 170, 170))
         gameover_screenfill.set_alpha(180)
-        self.screen.blit(gameover_screenfill, (0,0))
-        gameover = self.font_normalsize.render("Game Over", False, (255,255,255))
-        question = self.font_normalsize.render("to restart the game press Space", False, (255,255,255))
-        gameover_score = self.font_normalsize.render(f"Points:{self.points}", False, (255, 255, 255))
-        self.screen.blit(gameover, (
-            Settings.window_width // 2 - gameover.get_width() // 2, Settings.window_height // 2 - gameover.get_height() // 2))
-        self.screen.blit(question, (
-            Settings.window_width // 2 - question.get_width() // 2,
-            Settings.window_height // 2.1 - question.get_height() // 2.1))
-        self.screen.blit(gameover_score, (
-            Settings.window_width // 2 - gameover_score.get_width() // 2,
-            Settings.window_height // 2.2 - gameover_score.get_height() // 2.2))
-
+        self.screen.blit(gameover_screenfill, (0, 0))
+        gameover = self.font_normalsize.render("Game Over", False, (255, 255, 255))
+        question = self.font_normalsize.render(
+            "to restart the game press Space", False, (255, 255, 255)
+        )
+        gameover_score = self.font_normalsize.render(
+            f"Points:{self.points}", False, (255, 255, 255)
+        )
+        self.screen.blit(
+            gameover,
+            (
+                Settings.window_width // 2 - gameover.get_width() // 2,
+                Settings.window_height // 2 - gameover.get_height() // 2,
+            ),
+        )
+        self.screen.blit(
+            question,
+            (
+                Settings.window_width // 2 - question.get_width() // 2,
+                Settings.window_height // 2.1 - question.get_height() // 2.1,
+            ),
+        )
+        self.screen.blit(
+            gameover_score,
+            (
+                Settings.window_width // 2 - gameover_score.get_width() // 2,
+                Settings.window_height // 2.2 - gameover_score.get_height() // 2.2,
+            ),
+        )
 
     def watch_for_events(self):
         for event in pygame.event.get():
@@ -159,7 +206,9 @@ class Game(object):
                 for bubble in self.bubbles:
                     hits = bubble.rect.collidepoint(*pygame.mouse.get_pos())
                     if hits == True:
-                        burst_sound = pygame.mixer.Sound(os.path.join(Settings.path_sound, "Burst.mp3"))
+                        burst_sound = pygame.mixer.Sound(
+                            os.path.join(Settings.path_sound, "Burst.mp3")
+                        )
                         pygame.mixer.Sound.play(burst_sound)
                         self.points += bubble.scale["width"] // 2
                         bubble.kill()
@@ -172,7 +221,6 @@ class Game(object):
                     if self.gameover == True:
                         self.reset()
 
-
     def reset(self):
         self.points = 0
         self.bubbles.empty()
@@ -184,7 +232,7 @@ class Game(object):
         Getting highscore from file
         """
 
-        with open(Settings.path_highscore, 'r', encoding='utf8') as file:
+        with open(Settings.path_highscore, "r", encoding="utf8") as file:
             highscore = int(file.read())
 
         return highscore
@@ -195,7 +243,7 @@ class Game(object):
         Setting highscore to file
         """
 
-        with open(Settings.path_highscore, 'w', encoding='utf8') as file:
+        with open(Settings.path_highscore, "w", encoding="utf8") as file:
             file.write(str(highscore))
 
     def save_highscore(self) -> None:
@@ -210,9 +258,14 @@ class Game(object):
         self.t += 1
         if self.t >= Settings.timeunit:
             self.t = 0
-            if len(self.bubbles) <= (Settings.window_width + Settings.window_height) / 100:
+            if (
+                len(self.bubbles)
+                <= (Settings.window_width + Settings.window_height) / 100
+            ):
                 self.bubbles.add(Bubble("bubble.png", self))
-                spawn_sound = pygame.mixer.Sound(os.path.join(Settings.path_sound, "spawn.mp3"))
+                spawn_sound = pygame.mixer.Sound(
+                    os.path.join(Settings.path_sound, "spawn.mp3")
+                )
                 pygame.mixer.Sound.play(spawn_sound)
         self.t2 += 1
         if self.t2 >= Settings.timeunit / 2:
@@ -234,7 +287,9 @@ class Game(object):
         self.background.draw(self.screen)
         self.bubbles.draw(self.screen)
         self.mouse.draw(self.screen)
-        points = self.font_normalsize.render(f"Points: {self.points}", False, (255, 255, 255))
+        points = self.font_normalsize.render(
+            f"Points: {self.points}", False, (255, 255, 255)
+        )
         self.screen.blit(points, (0, 10))
 
 
